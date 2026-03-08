dd0eafb138dd7935c21652bed02d6104102cb34d
MlnCarlos <carlos_molina193@hotmail.com>
2026-03-08 00:20:52 -0500

    Organizando GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 import Home from "./pages/Home";
 import Services from "./pages/Services";
 import Pets from "./pages/Pets";
 import Contact from "./pages/Contact";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Navbar />
 
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/services" element={<Services />} />
         <Route path="/pets" element={<Pets />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
 
       <Footer />
-    </BrowserRouter>
+    </Router>
   );
 }
 
